d54816f9be481e1fe9573749a84d93dee006f55a
DESKTOP-1H1NVUN\user <sungyoungchoi94@gmail.com>
2022-08-12 00:06:46 +0900

    add categoryTemplate

---
+++ src/models/categoryTemplate.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+module.exports = class CategoryTemplate extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                type: {
+                    type: Sequelize.STRING(50),
+                    allowNull: false,
+                },
+                template: {
+                    type: Sequelize.STRING(500),
+                    allowNull: false,
+                },
+            },
+            {
+                sequelize,
+                timestamps: false,
+                modelName: 'CategoryTemplate',
+                tableName: 'category_template',
+                paranoid: false,
+                charset: 'utf8',
+                collate: 'utf8_general_ci',
+            }
+        );
+    }
+    static associate(db) {
+        db.CategoryTemplate.hasMany(db.CategoryTemplate, { foreignKey: 'categoryTemplateId' });
+}
+};
