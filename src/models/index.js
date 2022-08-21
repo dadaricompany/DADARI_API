72b18adc8af3413f7bdc9d5ffdea545d35396d98
sungyoung <tjddud117@naver.com>
2022-08-21 17:04:32 +0900

    fix : set db config with env

--- src/models/index.js
+++ src/models/index.js
@@ -11,6 +11,10 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../../config/config')[env]; // config/config.json에서 데이터베이스 설정을 불러옴
 const db = {};
 
+config.host = process.env.DB_HOST;
+config.username = process.env.DB_USERNAME;
+config.password = process.env.DB_PASSWORD;
+
 const sequelize = new Sequelize(config.database, config.username, config.password, config); // new Sequelize를 통해 MySQL 연결 객체 생성
 
 db.sequelize = sequelize; // 연결 객체를 나중에 재사용 하기 위해 db.Sequelize에 넣음
